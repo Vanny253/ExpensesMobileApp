31414be725d7c975a027f67a59c7746b5baa335f
Vanny253 <vannylai6011@gmail.com>
2025-11-25 20:50:36 +0800

    Menu navigation with header title

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,14 +4,17 @@ export default function RootLayout() {
   return (
     <Drawer
       screenOptions={{
-        headerShown: false,
+        headerShown: true,
         drawerType: "slide",
       }}
     >
       {/* Bottom Tabs */}
       <Drawer.Screen
         name="(tabs)"
-        options={{ drawerLabel: "Home" }}
+        options={{ 
+          drawerLabel: "Home",
+          headerShown: false,
+         }}
       />
 
       {/* Individual Drawer Screens */}
@@ -21,13 +24,35 @@ export default function RootLayout() {
           drawerItemStyle: { display: "none" },
         }}
       />
-      <Drawer.Screen name="(drawer)/category" options={{ drawerLabel: "Category" }} />
-      <Drawer.Screen name="(drawer)/reminder" options={{ drawerLabel: "Reminder" }} />
-      <Drawer.Screen name="(drawer)/regular_payment" options={{ drawerLabel: "Regular Payment" }} />
-      <Drawer.Screen name="(drawer)/budget" options={{ drawerLabel: "Budget" }} />
-      <Drawer.Screen name="(drawer)/export_data" options={{ drawerLabel: "Export Data" }} />
-      <Drawer.Screen name="(drawer)/about_us" options={{ drawerLabel: "About Us" }} />
-      <Drawer.Screen name="(drawer)/contact_us" options={{ drawerLabel: "Contact Us" }} />
+      <Drawer.Screen
+        name="(drawer)/category"
+        options={{ drawerLabel: "Category", title: "Category" }}
+      />
+      <Drawer.Screen
+        name="(drawer)/reminder"
+        options={{ drawerLabel: "Reminder", title: "Reminder" }}
+      />
+      <Drawer.Screen
+        name="(drawer)/regular_payment"
+        options={{ drawerLabel: "Regular Payment", title: "Regular Payment" }}
+      />
+      <Drawer.Screen
+        name="(drawer)/budget"
+        options={{ drawerLabel: "Budget", title: "Budget" }}
+      />
+      <Drawer.Screen
+        name="(drawer)/export_data"
+        options={{ drawerLabel: "Export Data", title: "Export Data" }}
+      />
+      <Drawer.Screen
+        name="(drawer)/about_us"
+        options={{ drawerLabel: "About Us", title: "About Us" }}
+      />
+      <Drawer.Screen
+        name="(drawer)/contact_us"
+        options={{ drawerLabel: "Contact Us", title: "Contact Us" }}
+      />
+
     </Drawer>
   );
 }
